e98763b97b089e41dfbf1d3c8897675468538b29
Natalya Babenko <natapervomaisk88@gmail.com>
2024-05-14 00:36:10 +0300

    added midleware

--- src/slices/todos/todos-slice.ts
+++ src/slices/todos/todos-slice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { TodoType } from "../../types/todo-type";
-import { act } from "react";
+
 const initialState: { todos: TodoType[] } = {
   todos: [],
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
 import userSlice from "../slices/user/user-slice";
 import todosSlice from "../slices/todos/todos-slice";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Tuple } from "@reduxjs/toolkit";
+import { loggerMiddleware } from "../middleware/logger";
+
 export const store = configureStore({
   reducer: {
     user: userSlice,
     todos: todosSlice,
   },
+  middleware: () => new Tuple(loggerMiddleware),
+  // middleware: [...additionalMiddleware, logger]
 });
